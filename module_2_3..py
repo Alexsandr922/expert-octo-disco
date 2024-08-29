f3ce8cb9eb6e18c8ba437499b3f88a570e431d79
Alexsandr922 <komogortsev1342@yandex.ru>
2024-08-29 15:02:37 +0800

    Add files via upload

---
+++ module_2_3..py
@@ -0,0 +1,27 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while my_list > [0]:
+    if my_list[0] > 0:
+        print(my_list[0])
+    if my_list[1] > 0:
+        print(my_list[1])
+        if my_list[2] > 0:
+            print(my_list[2])
+            if my_list[3] > 0:
+                print(my_list[3])
+                if my_list[5] > 0:
+                    print(my_list[5])
+                    if my_list[6] > 0:
+                        print(my_list[6])
+                        if my_list[7] > 0:
+                            print(my_list[7])
+                            if my_list[8] > 0:
+                                print(my_list[8])
+                                if my_list[9] > 0:
+                                    print(my_list[9])
+                                    if my_list[10] > 0:
+                                        print(my_list[10])
+
+    elif my_list[6] < 0:
+        print("отрицательное число")
+    break
